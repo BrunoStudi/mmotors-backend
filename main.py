024f74eaa6667ca2a6668d2e2f588fd79a98d63c
BrunoStudi <r.brunocarriere@protonmail.com>
2026-04-26 16:07:03 +0200

    CONFIG: autorisation CORS pour le frontend React

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from app.database import Base, engine
 from app.models import user
 from app.routes.auth import router as auth_router
 from app.routes.vehicle import router as vehicle_router
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
@@ -11,6 +12,17 @@ app = FastAPI(
     title="M-Motors API",
     description="API backend pour la plateforme M-Motors",
     version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(auth_router)
