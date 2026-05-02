30ba974f4ee9d158d1483466502ddcefd426062c
BrunoStudi <r.brunocarriere@protonmail.com>
2026-05-02 14:30:08 +0200

    CONFIG: ajout URL preview Vercel dans CORS

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "https://mmotors-frontend-8st1.vercel.app",
+        "https://mmotors-frontend-8st1-git-main-brunos-projects-853488ad.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
